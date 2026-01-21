a2a56ccef019b23decf5d8d3dbc464cff660c906
Arseniy Naumov <ivanov262ivanov@gmail.com>
2026-01-21 10:48:06 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import moviesFromServer from './api/movies.json';
 
-export const App = () => (
-  <div className="page">
+function prepareFilms(query){
+  let movies = moviesFromServer;
+  const normalizedQuery = query.trim().toLowerCase();
+  if(normalizedQuery){
+    movies = movies.filter((movie) => movie.title.includes(query) ||
+    movie.description.includes(query))
+  }
+ return movies;
+}
+export const App = () => {
+  const [query, setQuery] = useState('');
+  let visibleMovies = prepareFilms(query);
+ return(
+ <div className="page">
     <div className="page-content">
       <div className="box">
         <div className="field">
-          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
           <label htmlFor="search-query" className="label">
             Search movie
           </label>
@@ -18,6 +29,8 @@ export const App = () => (
               id="search-query"
               className="input"
               placeholder="Type search word"
+              value={query}
+              onChange={(event) => {setQuery(event.target.value)}}
             />
           </div>
         </div>
@@ -28,4 +41,5 @@ export const App = () => (
 
     <div className="sidebar">Sidebar goes here</div>
   </div>
-);
+  )
+};
